848c628e6deda211e8ea1c34f36b3c9b0a178737
ngala21 <ngalamike21@gmail.com>
2026-04-16 07:18:56 +0300

    salary function & logic works

--- js/app.js
+++ js/app.js
@@ -1,23 +1,34 @@
-// console.log("Hello World");
-const errorMessage = document.getElementById("salaryError");
-const expenditureValue = document.getElementById("rightAmount");
-const balanceValue = document.getElementById("balance");
-// Salary Column
-document.getElementById("saveSalary").onclick = () => {
-  let Salary = document.getElementById("salary").value;
+// console.log("Hello World"); => For testing purposes only
+let totalAmount = document.getElementById("total-amount");
+let userAmount = document.getElementById("user-amount");
+const checkAmountButton = document.getElementById("check-amount");
+const totalAmountButton = document.getElementById("total-amount-button");
+const productTitle = document.getElementById("product-title");
+const errorMessage = document.getElementById("budget-error");
+const productTitleError = document.getElementById("product-title-error");
+const productCostError = document.getElementById("product-cost-error");
+const amount = document.getElementById("amount");
+const expenditureValue = document.getElementById("expenditure-value");
+const balanceValue = document.getElementById("balance-amount");
+const list = document.getElementById("list");
+let tempAmount = 0;
 
-  // empty or negative input
-  if (Salary === "" || Salary < 0) {
+// Salary Column
+totalAmountButton.addEventListener("click", () => {
+  tempAmount = totalAmount.value;
+  //empty or negative input
+  if (tempAmount === "" || tempAmount < 0) {
     errorMessage.classList.remove("hide");
   } else {
     errorMessage.classList.add("hide");
+    //Set Budget
+    amount.innerHTML = tempAmount;
+    //Set Balance
+    balanceValue.innerText = tempAmount - expenditureValue.innerText;
+    //Clear Input Box
+    totalAmount.value = "";
   }
-
-  amount.innerHTML = Salary;
-
-  // Set Balance
-  balanceValue.innerText = Salary - expenditureValue.innerText;
-};
+});
 
 // document.getElementById();
 
